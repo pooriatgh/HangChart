be2d02aeb19b5d68a62d1534fbffefb4aac8178c
Pooria <taghizadeh@kahkeshan.com>
2020-07-21 22:47:53 +0430

    Init

---
+++ HangChart/HangChart/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HangChart.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
